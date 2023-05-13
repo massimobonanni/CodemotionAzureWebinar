f1950a84773e0bb0da129c8f051a3edfaee945a6
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-05-13 16:18:52 +0200

    Addc ognitive analyser configuration

---
+++ FeedbackManager.CognitiveServices/Configuration/CognitiveAnalyzerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackManager.CognitiveServices.Configuration
+{
+    internal class CognitiveAnalyzerConfiguration
+    {
+        public CognitiveAnalyzerConfiguration(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private readonly IConfiguration configuration;
+
+        public string? ApiKey { get; set; }
+        public string? Endpoint { get; set; }
+
+        public void Load()
+        {
+            ApiKey = configuration["CognitiveServices:ApiKey"];
+            Endpoint = configuration["CognitiveServices:Endpoint"];
+        }
+    }
+}
--- FeedbackManager.CognitiveServices/Services/CognitiveAnalyzer.cs
+++ FeedbackManager.CognitiveServices/Services/CognitiveAnalyzer.cs
@@ -1,5 +1,7 @@
-﻿using FeedbackManager.Core.Entities;
+﻿using FeedbackManager.CognitiveServices.Configuration;
+using FeedbackManager.Core.Entities;
 using FeedbackManager.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,20 @@ using System.Threading.Tasks;
 
 namespace FeedbackManager.CognitiveServices.Services
 {
+    /// <summary>
+    ///  More info about samples here:
+    ///  https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/textanalytics/Azure.AI.TextAnalytics/samples/README.md
+    /// </summary>
     public class CognitiveAnalyzer : IFeedbackAnalyzer
     {
+        private readonly CognitiveAnalyzerConfiguration configuration;
+
+        public CognitiveAnalyzer(IConfiguration configuration)
+        {
+            this.configuration=new CognitiveAnalyzerConfiguration(configuration);
+
+            this.configuration.Load();
+        }
 
         public Task<FeedbackReport> AnalyzeAsync(FeedbackData feedback, CancellationToken token = default)
         {
