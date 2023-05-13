a48e2b8075d88556830bcbb28889fe979bb83d11
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-05-13 16:02:20 +0200

    Creation of the Cognitive Analyzer for sentiment analysis

---
+++ FeedbackManager.CognitiveServices/Services/CognitiveAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using FeedbackManager.Core.Entities;
+using FeedbackManager.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackManager.CognitiveServices.Services
+{
+    public class CognitiveAnalyzer : IFeedbackAnalyzer
+    {
+
+        public Task<FeedbackReport> AnalyzeAsync(FeedbackData feedback, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
