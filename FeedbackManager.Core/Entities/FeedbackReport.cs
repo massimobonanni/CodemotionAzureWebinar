4fe912fbddc27fe8a85f36af7e9a065f6ce35e2d
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-05-15 13:13:49 +0200

    Add file to ignore definition

--- FeedbackManager.Core/Entities/FeedbackReport.cs
+++ FeedbackManager.Core/Entities/FeedbackReport.cs
@@ -4,11 +4,12 @@
     {
         public FeedbackReport(FeedbackData feedback)
         {
+            this.Id=Guid.NewGuid();
             this.Feedback = feedback;
         }
 
         public FeedbackData Feedback { get; set; }
-
+        public Guid Id { get; set; }
         public string? Language { get; set; }
         public Sentiment Sentiment { get; set; }
         public double SentimentConfidence { get; set; }
--- FeedbackManager.Web/Controllers/HomeController.cs
+++ FeedbackManager.Web/Controllers/HomeController.cs
@@ -17,12 +17,8 @@ namespace FeedbackManager.Web.Controllers
             _feedbackAnalyzer = feedbackAnalyzer;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var a = await this._feedbackAnalyzer.AnalyzeAsync(
-                new Core.Entities.FeedbackData("mabonann@microsoft.com",
-                    "Fantastico prodotto da provare"));
-
             return View();
         }
 
