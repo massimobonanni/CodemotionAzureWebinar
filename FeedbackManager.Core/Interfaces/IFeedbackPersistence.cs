13807e4bc4b3f41305a8a721c0b3e322c5667953
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-05-16 16:34:06 +0200

    Add persistance interface

---
+++ FeedbackManager.Core/Interfaces/IFeedbackPersistence.cs
@@ -0,0 +1,14 @@
+﻿using FeedbackManager.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackManager.Core.Interfaces
+{
+    public interface IFeedbackPersistence
+    {
+        Task<bool>  InsertFeedbackReportAync(FeedbackReport feedbackReport, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
