36ade5496af68571e916e292e9fbef03abdd5bdf
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-05-30 15:24:03 +0200

    Add opinion minimg to cognitive and textarea for feedback

--- FeedbackManager.CognitiveServices/Services/CognitiveAnalyzer.cs
+++ FeedbackManager.CognitiveServices/Services/CognitiveAnalyzer.cs
@@ -39,14 +39,15 @@ namespace FeedbackManager.CognitiveServices.Services
             Uri endpoint = new(this.configuration.Endpoint);
             AzureKeyCredential credential = new(this.configuration.ApiKey);
             TextAnalyticsClient client = new(endpoint, credential);
-
+            
             try
             {
                 Response<DetectedLanguage> languageResponse = await client.DetectLanguageAsync(feedback.Text);
-
+               
                 response.Language = languageResponse.Value.Name;
 
-                Response<DocumentSentiment> sentimentResponse = await client.AnalyzeSentimentAsync(feedback.Text,languageResponse.Value.Iso6391Name);
+                AnalyzeSentimentOptions options = new() { IncludeOpinionMining = true };
+                Response<DocumentSentiment> sentimentResponse = await client.AnalyzeSentimentAsync(feedback.Text,languageResponse.Value.Iso6391Name, options);
 
                 response.Sentiment = sentimentResponse.Value.ToSentiment();
                 response.SentimentConfidence = sentimentResponse.Value.GetConfidence();
