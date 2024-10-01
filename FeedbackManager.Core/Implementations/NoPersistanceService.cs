6e1af715a36fee383152125166c748f2f63c1cb4
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-10-01 15:50:33 +0200

    Add no persistance service. Persistance service that doesn't write enything

---
+++ FeedbackManager.Core/Implementations/NoPersistanceService.cs
@@ -0,0 +1,18 @@
+﻿using FeedbackManager.Core.Entities;
+using FeedbackManager.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackManager.Core.Implementations
+{
+    public class NoPersistanceService : IFeedbackPersistence
+    {
+        public Task<bool> InsertFeedbackReportAync(FeedbackReport feedbackReport, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
