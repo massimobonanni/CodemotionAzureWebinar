65343e2a03d681b2e95aed0ee62a274d7f83e32e
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-10-01 16:43:34 +0200

    Add service configuration page

---
+++ FeedbackManager.Web/Controllers/ServiceInfoController.cs
@@ -0,0 +1,25 @@
+﻿using FeedbackManager.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackManager.Web.Controllers
+{
+    public class ServiceInfoController : Controller
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ServiceInfoController(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IActionResult Index()
+        {
+            var feedbackAnalyzer = serviceProvider.GetService<IFeedbackAnalyzer>();
+            this.ViewBag.FeedbackAnalyzer = feedbackAnalyzer?.GetType().FullName;
+            var feedbackPersistence = serviceProvider.GetService<IFeedbackPersistence>();
+            this.ViewBag.FeedbackPersistence = feedbackPersistence?.GetType().FullName;
+
+            return View();
+        }
+    }
+}
